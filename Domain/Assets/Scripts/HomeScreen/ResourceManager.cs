064bafb962e8b378302a586b61852e19409fb6b9
neilchowCS <neilxchow@gmail.com>
2022-06-12 17:06:34 -0700

    time based resource accumulation

--- Domain/Assets/Scripts/HomeScreen/ResourceManager.cs
+++ Domain/Assets/Scripts/HomeScreen/ResourceManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ResourceManager : MonoBehaviour
 {
     public ResourceHandler resourceHandler;
-    public System.DateTime dateTime;
+    public TextMeshProUGUI availableText;
 
+    public System.DateTime dateTime;
+    private float goldPerMin = 12.5f;
+    private float essencePerMin = 19;
+
+    float elapsed = 1f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +23,48 @@ public class ResourceManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        elapsed += Time.deltaTime;
+        if (elapsed >= 1f)
+        {
+            elapsed = elapsed % 1f;
+            DisplayAvailableReward();
+        }
     }
 
     public void ClaimReward()
     {
-        resourceHandler.playerData.lastClaim = System.DateTime.UtcNow.ToString();
-        resourceHandler.playerData.gold += 1000;
-        resourceHandler.playerData.essence += 1000;
+        (int, int, int) reward = CalcReward();
+
+        resourceHandler.playerData.gold += reward.Item1;
+        resourceHandler.playerData.essence += reward.Item2;
+        resourceHandler.playerData.lastClaim = (System.DateTime.UtcNow.AddSeconds(-reward.Item3)).ToString();
         resourceHandler.WritePlayerData();
+
+        DisplayAvailableReward();
+    }
+
+    public (int, int, int) CalcReward()
+    {
+        System.TimeSpan timeSpan = (System.DateTime.UtcNow -
+            System.DateTime.Parse(resourceHandler.playerData.lastClaim));
+        int numMin = Mathf.Min((int)(timeSpan.TotalMinutes), 480);
+        int overflow = 0;
+        if (numMin != 480)
+        {
+            overflow = timeSpan.Seconds;
+        }
+        return ((int)(numMin * goldPerMin), (int)(numMin * essencePerMin), overflow);
+    }
+
+    public void DisplayAvailableReward()
+    {
+        (int, int, int) reward = CalcReward();
+
+        availableText.text = $"Gold: {reward.Item1}\nEssence: {reward.Item2}";
+    }
+
+    public void DisplayAvailableReward((int, int, int) prevCalc)
+    {
+        availableText.text = $"Gold: {prevCalc.Item1}\nEssence: {prevCalc.Item2}";
     }
 }
